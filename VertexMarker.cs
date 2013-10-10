f3c53f589b8226e32e8c41692d47c50b4b81e99f
Eiichi Takebuchi <nanashi4129@gmail.com>
2013-10-10 21:44:06 +0900

    色々と変更

--- VertexMarker.cs
+++ VertexMarker.cs
@@ -1,39 +1,86 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 /// <summary>
-/// メッシュ付きのGameObjectの頂点にCubeのマーカーを付ける
+/// 頂点を表示したいGameObjectにアサインする
+/// ただし，対象のGameObjectにはSkinnedMeshRendererがあること
 /// </summary>
-public class VertexMarker 
+public class VertexMarker : MonoBehaviour
 {
-	public static void AssignMarker(GameObject root, Mesh mesh, Vector3 marker_size)
+	public GameObject marker;
+	public float marker_size = 0.01f;
+
+	float prev_marker_size;
+	Mesh target_mesh;
+
+	void Start()
 	{
-		var primitive_mesh = GetPrimitivesSharedMesh();
-		var marker_branch = CreateBranchedMarkerObject(root);
-		StructureVertexMarkers(mesh, marker_branch, primitive_mesh, marker_size);
+		var target = gameObject;
+
+		target_mesh = target.GetComponent<SkinnedMeshRenderer>().sharedMesh;
+		
+		AssignMarker(target, marker_size);
+
+		prev_marker_size = marker_size;
 	}
 
-	static void StructureVertexMarkers(Mesh root_mesh, GameObject marker_branch, Mesh primitive_mesh, Vector3 marker_size)
+	void Update()
 	{
-		for (int i = 0; i < root_mesh.vertices.Length; i++)
+		if (marker_size != prev_marker_size)
 		{
-			var obj = new GameObject(i.ToString());
-			var transform = obj.transform;
-			transform.parent = marker_branch.transform;
-			transform.localPosition = root_mesh.vertices[i];
-			transform.localScale = marker_size;
-			obj.AddComponent<MeshFilter>().sharedMesh = primitive_mesh;
-			obj.AddComponent<MeshRenderer>().material = new Material("Unlit/Masked Colored");
+
+		}
+		prev_marker_size = marker_size;
+	}
+
+	Mesh CopyMesh(GameObject marker)
+	{
+		var mesh = marker.GetComponent<MeshFilter>().sharedMesh;
+		var new_mesh = new Mesh();
+
+		new_mesh.vertices = new Vector3[mesh.vertexCount];
+		for (int i = 0; i < mesh.vertexCount; i++)
+			new_mesh.vertices[i] = mesh.vertices[i];
+
+		new_mesh.triangles = new int[mesh.triangles.Length];
+		for (int i = 0; i < mesh.triangles.Length; i++)
+			new_mesh.triangles[i] = mesh.triangles[i];
+
+		new_mesh.normals = new Vector3[mesh.normals.Length];
+		for (int i = 0; i < mesh.normals.Length; i++)
+			new_mesh.normals[i] = mesh.normals[i];
+
+		return new_mesh;
+	}
+
+	void StructureVertexMarkers(Transform marker_branch_transform, float marker_size)
+	{
+		
+
+		for (int i = 0; i < target_mesh.vertices.Length; i++)
+		{
+			InstantiateMarker(marker, marker_branch_transform, target_mesh.vertices[i], marker_size);
 		}
 	}
 
-	static Mesh GetPrimitivesSharedMesh()
+	void AssignMarker(GameObject root, float marker_size)
 	{
-		var primitive = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		return primitive.GetComponent<MeshFilter>().sharedMesh;
+		var marker_branch = CreateBranchedMarkerObject(root);
+		StructureVertexMarkers(marker_branch.transform, marker_size);
 	}
 
-	static GameObject CreateBranchedMarkerObject(GameObject root)
+	public void InstantiateMarker(GameObject marker, Transform branch, Vector3 mesh_vtx, float size)
+	{
+		var obj = Instantiate(marker) as GameObject;
+		var transform = obj.transform;
+		transform.parent = branch;
+		transform.localPosition = mesh_vtx;
+
+		//transform.localScale = Vector3.one * size;
+	}
+
+	GameObject CreateBranchedMarkerObject(GameObject root)
 	{
 		var marker_branch = new GameObject("markers");
 		marker_branch.transform.parent = root.transform;
